1bcbf32c5c8e52332444a514cab8e65c38921df5
happyleaf <witcxc@gmail.com>
2018-01-16 21:58:50 +0800

    fix err

--- ztree_toc.js
+++ ztree_toc.js
@@ -58,6 +58,11 @@ function factor(opts ,count,current) {
 	 * 根据header创建目录内容
 	 */
 	function create_toc(opts) {
+        opts._header_nodes = [{ id:1, pId:0, name:"Table of Content",open:true}]
+		opts._header_offsets = []
+		//opts._zTree = null
+		opts._headers = []
+
 		$(opts.documment_selector).find(':header').each(function() {
 			var level = parseInt(this.nodeName.substring(1), 10);
 
